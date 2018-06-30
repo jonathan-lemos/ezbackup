1a4724c5f4190b0f19b8fdf95211cd4d269ca191
Jonathan Lemos <n01367640@unf.edu>
2018-06-29 20:01:02 -0400

    backup test wip

--- tests/backup_test.c
+++ tests/backup_test.c
@@ -1,11 +1,21 @@
 #include "test_base.h"
 #include "../backup.h"
+#include "../options/options.h"
 #include "../log.h"
+#include "../cloud/keys.h"
 
 void test_backup(enum TEST_STATUS* status){
-	TEST_ASSERT(1);
+	struct options* opt = NULL;
+
+	opt = options_new();
+	TEST_ASSERT(opt);
+
+	co_set_username(opt->cloud_options, MEGA_SAMPLE_USERNAME);
+	co_set_password(opt->cloud_options, MEGA_SAMPLE_PASSWORD);
+	opt->cloud_options->cp = CLOUD_MEGA;
+
 cleanup:
-	;
+	opt ? options_free(opt) : (void)0;
 }
 
 int main(void){
